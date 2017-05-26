5eb8bd67bf8e261fc768758d05abfd6716beef58
Scientific name: dasypodidae <clarajchu@gmail.com>
2017-05-25 21:49:54 -0400

    Update Food.java

--- Food.java
+++ Food.java
@@ -1,65 +1,21 @@
-package meal;
+package meal2;
 
-import java.util.Scanner;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Random;
 
 public abstract class Food {
- 
-  public static int sumCal(ArrayList<Food> list) {
-    int calories = 0;
-    for ( Food yumyum : list) {
-      if (yumyum instanceof Tomato) {
-        Tomato a = (Tomato) yumyum;
-        calories += a.getCal();
-      }
-      if (yumyum instanceof Onion) {
-        Onion a = (Onion) yumyum;
-        calories += a.getCal();
-      }
-    }
-    return calories;
-  }
-
- public static void main (String[] args) {
-  
-  ArrayList <Food> foodList = new ArrayList <Food> ();
-   
-  Scanner scan = new Scanner(System.in);
-  System.out.println("How many tomatoes will you add?");
-  int numTom = scan.nextInt();
-  if (numTom > 0) {
-    System.out.println("Are you making tomato sauce?");
-    String sauceResponse = scan.next();
-    String sauceResD = sauceResponse.toLowerCase();
-    
-    for (int i = 0; i < numTom; i++) {
-   Tomato tom = new Tomato();
-   if (sauceResD.contains("y")) tom.makeSauce();
-   foodList.add(tom);
-    }
-  }
-  
-  System.out.println("How many onions will you add?");
-  int numOni = scan.nextInt();
-  if (numOni > 0) {
-    System.out.println("Would you like them roasted or fried? Or would you prefer them raw?");
-    String oniResponse = scan.next();
-    scan.close();
-    String oniResD = oniResponse.toLowerCase();
-     for (int i = 0; i < numOni; i++) {
-       Onion oni = new Onion();
-       if (oniResD.contains("roa")) oni.getRoasted();
-       else if (oniResD.contains("fri")) oni.getFried();        
-       foodList.add(oni);
-     }
-  }
-  System.out.println();
-  System.out.println("LIST of INGREDIENTS:");
-  System.out.println(foodList);
-  System.out.println();
-  System.out.println("TOTAL CALORIES in meal : " + sumCal(foodList) +" enjoy ;)");
-
- }
+	protected int calories;
+	protected int weight;
+	protected String flavor;
+	protected static Random ranGen = new Random();
+	
+	  public int getCal() {
+		    return this.calories;
+		  }
+		  
+	  public int getWeight() {
+		    return this.weight;
+		  }
+	  
+	  public abstract String toString();
 
 }
